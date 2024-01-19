95d8199fdee3445a7f6bfe76f4bf6a7fa555300d
KRobertK13 <102753849+KRobertK13@users.noreply.github.com>
2024-01-19 14:05:40 +0100

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,54 +1,74 @@
-import numpy as np #(működik a Moodle-ben is)
-
+import numpy as np
 
 ######################## 1. feladat, entrópiaszámítás #########################
 def get_entropy(n_cat1: int, n_cat2: int) -> float:
-    prob_1 = n_cat1/(n_cat1+n_cat2)
-    prob_2 = n_cat2/(n_cat1+n_cat2)
     if n_cat1 == 0 or n_cat2 == 0:
         return 0
-    entropy = - (prob_1 * np.log2(prob_1) + prob_2 * np.log2(prob_2))
-    return entropy
+    val_n_cat1 = n_cat1 / (n_cat1 + n_cat2)
+    val_n_cat2 = n_cat2 / (n_cat1 + n_cat2)
+    return -val_n_cat1 * np.log2(val_n_cat1) - val_n_cat2 * np.log2(val_n_cat2)
+
 ###################### 2. feladat, optimális szeparáció #######################
 def get_best_separation(features: list, labels: list) -> (int, int):
     best_separation_feature, best_separation_value = 0, 0
-    min = float("inf")
-    #TODO számítsa ki a legjobb szeparáció tulajdonságát és értékét!
-    features = np.array(features)
-    for j in range(len(features[0])-1):
-        actual = features[0][j]
-        en = 0
-        ep = 0
-        fn = 0
-        fp = 0
-        ln = 0
-        lp = 0
-        for i in range(len(features)-1):
-            for k in features[i]:
-                if k <= actual:
-                    if labels[i] == 1: 
-                        ep += 1
-                        lp += 1
-                    else: 
-                        en += 1
-                        ln += 1
-                else:
-                    if labels[i] == 1: 
-                        fp += 1
-                        lp += 1
-                    else: 
-                        fn += 1
-                        ln += 1
-        l = ep+en+fp+fn
-        efficiency = get_entropy(l,l) - ((ep+en) / l * get_entropy(ep, l) + (fp+fn) / l *get_entropy(fp,l))
-        if efficiency < min: 
-            min = efficiency
-            best_separation_feature = features[0][j]
-            best_separation_value = j
-    return best_separation_feature, best_separation_value        
+    min_ent = float('inf')
+
+    for fi in range(len(features[0])):
+        features_sublist = [s[fi] for s in features]
+        distinct = set(features_sublist)
+        for fv in distinct:
+            smaller = [labels[i] for i in range(len(labels)) if features[i][fi] <= fv]
+            bigger = [labels[i] for i in range(len(labels)) if features[i][fi] > fv]
+            ent = (len(smaller) / len(labels)) * get_entropy(smaller.count(0), smaller.count(1)) + \
+                  (len(bigger) / len(labels)) * get_entropy(bigger.count(0), bigger.count(1))
+            if ent < min_ent:
+                min_ent = ent
+                best_separation_feature = fi
+                best_separation_value = fv
+    return best_separation_feature, best_separation_value
+
 ################### 3. feladat, döntési fa implementációja ####################
 def main():
-    #TODO: implementálja a döntési fa tanulását!
+    tdat = np.loadtxt('train.csv', delimiter=',')
+    tlab = tdat[:, 8]
+    tfea = tdat[:, :7]
+    tree = tree_train(tfea, tlab)
+    cdat = np.loadtxt('test.csv', delimiter=',')
+    results = []
+    for i in cdat:
+        results.append(evaluate(tree, i))
+    np.savetxt('results.csv', results, fmt='%d')
     return 0
+
+def tree_train(features: list, labels: list):
+    if len(set(labels)) == 1: return labels[0]
+    if len(features[0]) == 0: return max(set(labels), key=labels.count)
+    bv = get_best_separation(features, labels)
+    lf = []
+    for i in features:
+        if i[bv[0]] <= bv[1]:
+            lf.append(i)
+    rf = []
+    for i in features:
+        if i[bv[0]] > bv[1]:
+            rf.append(i)
+    ll = []
+    for i in range(len(labels)):
+        if features[i][bv[0]] <= bv[1]:
+            ll.append(labels[i])
+    rl = []
+    for i in range(len(labels)):
+        if features[i][bv[0]] > bv[1]:
+            rl.append(labels[i])
+    return bv[0], bv[1], tree_train(lf, ll), tree_train(rf, rl)
+
+def evaluate(tree, value) -> int:
+    if tree == 0 or tree == 1:
+        return tree;
+    if value[tree[0]] <= tree[1]:
+        return evaluate(tree[2], value)
+    else:
+        return evaluate(tree[3], value)
+
 if __name__ == "__main__":
     main()
